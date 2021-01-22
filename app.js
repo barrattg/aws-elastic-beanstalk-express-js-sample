3adbda9e62d0bcda8b1c5e362580c4b70f79c489
Gregory Barratt <46229696+barrattg@users.noreply.github.com>
2021-01-22 10:29:59 +0000

    Full pipeline test

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Edit Test'));
+app.get('/', (req, res) => res.send('Edit Test 2'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
